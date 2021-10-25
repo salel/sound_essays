8f3b9ecceafb7844a68ec7ae227af83951dfabcd
salel <leluronsarah@gmail.com>
2021-10-25 03:48:04 +0200

    mad fx + save to wav

--- test.cpp
+++ test.cpp
@@ -4,6 +4,9 @@
 #define _USE_MATH_DEFINES
 #include <cmath>
 #include <string>
+#include <fstream>
+#include <csignal>
+#include <thread>
 
 #define PCM_DEVICE "default"
 
@@ -21,11 +24,19 @@ float t_freq(float t, float freq) {
 }
 
 float square_wave(float t) {
-    return t<0.5?-1:1;
+    return t<0.5?1:-1;
 }
 
 float sine_wave(float t) {
     return sin(t*M_PI);
+}
+
+float saw_wave(float t) {
+    return t*2-1;
+}
+
+float triangle_wave(float t) {
+    return t<0.5?t*4-1:3-t*4;
 }
 
 int16_t convert(float s, float volume) {
@@ -58,7 +69,48 @@ int keyboard_note_index(const char* s) {
     return index;
 }
 
+vector<int16_t> full_buffer;
+
+// on sigint save wav
+void signalHandler(int signum) {
+    cout << full_buffer.size() << endl;
+
+    ofstream file("output.wav", ios::out | ios::binary);
+
+    file << "RIFF";
+
+    int32_t file_size = full_buffer.size()*2 + 44;
+    int32_t fmt_len = 16;
+    int16_t fmt_type = 1;
+    int16_t fmt_channels = 1;
+    int32_t fmt_rate = 48000;
+    int32_t fmt_bits_per_sample = 16;
+    int32_t fmt_bytes_per_sample = fmt_bits_per_sample*fmt_channels/8;
+    int32_t fmt_bytes_sec = fmt_rate*fmt_bytes_per_sample;
+    int32_t data_size = full_buffer.size()*2;
+
+    file.write((char*)&file_size, sizeof(int32_t));
+    file << "WAVEfmt ";
+    file.write((char*)&fmt_len, sizeof(int32_t));
+    file.write((char*)&fmt_type, sizeof(int16_t));
+    file.write((char*)&fmt_channels, sizeof(int16_t));
+    file.write((char*)&fmt_rate, sizeof(int32_t));
+    file.write((char*)&fmt_bytes_sec, sizeof(int32_t));
+    file.write((char*)&fmt_bytes_per_sample, sizeof(int16_t));
+    file.write((char*)&fmt_bits_per_sample, sizeof(int16_t));
+    file << "data";
+    file.write((char*)&data_size, sizeof(int32_t));
+
+    file.write((char*)full_buffer.data(), full_buffer.size()*sizeof(int16_t));
+
+    file.flush();
+
+    exit(0);
+}
+
 int main() {
+    signal(SIGINT, signalHandler);
+
     // Initialize the shits
     unsigned int rate = 48000;
     unsigned int channels = 1;
@@ -84,18 +136,21 @@ int main() {
     error(snd_pcm_hw_params_set_rate_near(
         pcm_handle, params, &rate, 0));
 
-    error(snd_pcm_hw_params(pcm_handle,params));
+    unsigned int periods = 4;
 
-    cout << snd_pcm_name(pcm_handle) << endl;
-    cout << snd_pcm_state_name(snd_pcm_state(pcm_handle)) << endl;
+    error(snd_pcm_hw_params_set_periods_near(pcm_handle, params, &periods, 0));
+
+    unsigned int period_time = 5e5;
+
+    error(snd_pcm_hw_params_set_period_time(pcm_handle, params, period_time, 0));
+
+    error(snd_pcm_hw_params(pcm_handle, params));
 
     snd_pcm_hw_params_get_channels(params, &channels);
     snd_pcm_hw_params_get_rate(params, &rate, 0);
 
     snd_pcm_uframes_t frames;
     snd_pcm_hw_params_get_period_size(params, &frames, 0);
-
-    cout << frames << endl;
 
     vector<int16_t> buffer(frames * channels);
 
@@ -105,19 +160,27 @@ int main() {
     const auto kb = gen_keyboard();
 
     for (int loop = 0; true; loop++) {
+        cout << loop << endl;
 
         for (size_t i=0;i<buffer.size();i++) {
             float t = (float)(loop*frames+i)/(float)rate;
 
+            /*
             float val = 
-                sine_wave(t_freq(t, kb[keyboard_note_index("A2")]))+
+                triangle_wave(t_freq(t, kb[keyboard_note_index("A2")]))
+                /*
                 sine_wave(t_freq(t, kb[keyboard_note_index("A3")]))+
                 sine_wave(t_freq(t, kb[keyboard_note_index("E3")]))+
                 sine_wave(t_freq(t, kb[keyboard_note_index("A4")]))+
-                sine_wave(t_freq(t, kb[keyboard_note_index("C4")]));
+                sine_wave(t_freq(t, kb[keyboard_note_index("C4")]))*/;
+
+            float freq = pow(220, 1.0+t*0.1);
+            float val = square_wave(t_freq(t,freq));
 
             buffer[i] = convert(val, volume);
         }
+
+        full_buffer.insert(full_buffer.end(), buffer.begin(), buffer.end());
 
         int result = snd_pcm_writei(pcm_handle, buffer.data(), frames);
         error(result);
